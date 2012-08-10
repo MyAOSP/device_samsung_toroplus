eafca3c60a113aa0680ea3612b6d621cf2df0ebe
travp624 <travp624@gmail.com>
2012-08-09 22:02:09 -0500

    working on fixing toroplus

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,6 +30,14 @@ PRODUCT_COPY_FILES += \
     device/samsung/toroplus/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/samsung/toroplus/plus-apns-conf.xml:system/etc/apns-conf.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := toroplus
+PRODUCT_NAME := full_toroplus
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
